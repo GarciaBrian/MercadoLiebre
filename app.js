e8fa4a22bf9328b80fa5a11202190c4dc9b36963
Brian <garciabrianalejandro@gmail.com>
2021-11-11 00:14:25 -0300

    subo formulario y login

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ app.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname,"views/index.html"))
 })
 
+app.get('/register', (req,res)=>{
+    res.sendFile(path.join(__dirname,"views/register.html"))
+})
+
+app.get('/login', (req,res)=>{
+    res.sendFile(path.join(__dirname,"views/login.html"))
+})
+
 app.listen(PORT, () => console.log(`El servidor esta escuchando en el
 puerto ${PORT}
 http://localhost:${PORT}`));
